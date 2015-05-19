a8de1b679060458b89faa20f57966c8141d1ff9b
Lazlo Mita <lazlo.mita@gmail.com>
2015-05-18 21:01:12 -0400

    another change

--- js/events-tutorial.js
+++ js/events-tutorial.js
@@ -3,11 +3,46 @@ var mapOptions = {
   zoom: 5
 };
 
+var mapStyle = [
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "saturation": 37 },
+      { "color": "#5f5f60" },
+      { "weight": 1.9 },
+      { "lightness": -29 }
+    ]
+  },{
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "visibility": "on" },
+      { "weight": 1.5 },
+      { "color": "#94371e" }
+    ]
+  },{
+    "featureType": "road.highway",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#2447b5" },
+      { "saturation": -64 },
+      { "lightness": 17 }
+    ]
+  }
+];
+
 var image1 = {
   url: 'images/marker-red.png',
-  size: new google.maps.Size(32, 32),
+  size: new google.maps.Size(12, 12),
   origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(16, 30),
+  anchor: new google.maps.Point(6, 12),
   scaledSize: new google.maps.Size(12, 12)
 };
 var image2 = {
@@ -27,8 +62,9 @@ var shape2 = {
     type: 'poly'
 };
 
-var map;
+var map = null;
 var allMapMarkers = [];
+var infowindow = null;
 
 
 
@@ -36,18 +72,24 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   setMarkers(haircuteryLocations);
   google.maps.event.addListener(map, 'zoom_changed', function() {
-    if ( this.getZoom() > 12) {
-      for (var i = 0; i < allMapMarkers.length; i++) {
-        allMapMarkers[i].setIcon(image2);
-        allMapMarkers[i].setShape(shape2);
-      }
-    } else {
-      for (var i = 0; i < allMapMarkers.length; i++) {
-        allMapMarkers[i].setIcon(image1);
-        allMapMarkers[i].setShape(shape1);
-      }
-    }
+    applyMarkerImage();
   });
+  // Map settings
+  map.set('styles', mapStyle);
+}
+
+function applyMarkerImage() {
+  if ( map.getZoom() > 12) {
+    for (var i = 0; i < allMapMarkers.length; i++) {
+      allMapMarkers[i].setIcon(image2);
+      allMapMarkers[i].setShape(shape2);
+    }
+  } else {
+    for (var i = 0; i < allMapMarkers.length; i++) {
+      allMapMarkers[i].setIcon(image1);
+      allMapMarkers[i].setShape(shape1);
+    }
+  }
 }
 
 function setMarkers(locations) {
@@ -70,15 +112,36 @@ function setMarkers(locations) {
         phone: location[6]
     });
     allMapMarkers.push(marker);
-
     google.maps.event.addListener(marker, 'click', function() {
+      if (infowindow != null) {
+        infowindow.close();
+      }
+      applyMarkerImage();
       // map.setZoom(8);
       // map.setCenter(this.getPosition());
       if (map.getZoom()<=12) {
         this.setIcon(image2);
+        this.setPosition(this.getPosition());
         this.setShape(shape2);
         this.setZIndex(4);
       }
+      var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h2 id="firstHeading" class="firstHeading">'+this.storeName+'</h2>'+
+            '<div id="bodyContent">'+
+            '<p><b>'+this.street+'</b>, '+this.city+' <b>'+this.state+'</b>'+
+            '<p>Phone: '+this.phone+'</p>'+
+            '</div>'+
+            '</div>';
+
+      infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth:180,
+            pixelOffset:0,
+            zIndex: 2
+          });
+      infowindow.open(map,this);
     });
 
   }
